63486e34a1171680500fede7b7e092511e5eb89f
Anja Kammer <post@anjakammer.de>
2017-10-25 23:43:36 +0200

    deleted unused routes #TDB-2 #TDB-9

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :stories do
-    resources :tasks, only: [:index, :new, :create]
+  resources :stories, only: [:index, :show, :update, :destroy] do
+    resources :tasks, only: [:index, :create]
   end
-  resources :tasks, only: [:index, :show, :edit, :update, :destroy]
+  resources :tasks, only: [:index, :show, :update, :destroy]
 
 end
