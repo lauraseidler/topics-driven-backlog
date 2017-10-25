d9be21caaedf32b0320d67b427688c6c21f04bdc
Anja Kammer <post@anjakammer.de>
2017-10-25 23:28:47 +0200

    enhanced and decoupled routes #TDB-2 #TDB-9

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+
   resources :stories do
-    resources :tasks
+    resources :tasks, only: [:index, :new, :create]
   end
+  resources :tasks, only: [:index, :show, :edit, :update, :destroy]
+
 end
