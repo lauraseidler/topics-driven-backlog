006b2e556cdc2050330e980a6566fbc2621c0c3b
Anja Kammer <post@anjakammer.de>
2017-10-26 00:13:23 +0200

    fixed task routes according to new structure #TDB-2 #TDB-9

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,30 +1,37 @@
 class TasksController < ApplicationController
-  before_action :set_story
+  before_action :set_story, only: [:create]
   before_action :set_story_task, only: [:show, :update, :destroy]
 
   # GET /stories/:story_id/tasks
+  # GET /tasks
   def index
-    json_response(@story.tasks)
+    if params.has_key?(:story_id)
+      set_story
+      json_response(@story.tasks)
+    else
+      json_response(Task.all)
+    end
   end
 
-  # GET /stories/:story_id/tasks/:id
+  # GET /stories/:story_id/tasks
+  # GET /tasks/:id
   def show
     json_response(@task)
   end
 
   # POST /stories/:story_id/tasks
   def create
-    @story.tasks.create!(task_params)
-    json_response(@story, :created)
+    @task = @story.tasks.create!(task_params)
+    json_response(@task, :created)
   end
 
-  # PUT /stories/:story_id/tasks/:id
+  # PUT /tasks/:id
   def update
     @task.update(task_params)
     head :no_content
   end
 
-  # DELETE /stories/:story_id/tasks/:id
+  # DELETE /tasks/:id
   def destroy
     @task.destroy
     head :no_content
@@ -41,7 +48,11 @@ class TasksController < ApplicationController
   end
 
   def set_story_task
-    @task = @story.tasks.find_by!(id: params[:id]) if @story
+    if @story
+      @task = @story.tasks.find_by!(id: params[:id])
+    else
+      @task = Task.find(params[:id])
+    end
   end
-  
+
 end
