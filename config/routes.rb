b084c8a0f341c583bd6d78a1b0f3fe8834305ebf
Anja Kammer <post@anjakammer.de>
2017-10-26 00:17:00 +0200

    readded create route for stories #TDB-2 #TDB-9

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :stories, only: [:index, :show, :update, :destroy] do
+  resources :stories, only: [:index, :create, :show, :update, :destroy] do
     resources :tasks, only: [:index, :create]
   end
   resources :tasks, only: [:index, :show, :update, :destroy]
