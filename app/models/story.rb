0ad1aa70bdc0b7fdf8a11d91b750d51f34824b5e
Anja Kammer <post@anjakammer.de>
2017-11-10 00:20:10 +0100

    added status field with default value #TDB-26

--- app/models/story.rb
+++ app/models/story.rb
@@ -2,5 +2,9 @@ class Story < ApplicationRecord
   has_many :tasks, dependent: :destroy
   acts_as_list
 
+  def self.statuses
+    {:open => 0, :progressing => 1, :closed => 2}
+  end
+
   validates_presence_of :title
 end
