9dd5a331e72777c28c5abbbd49e2e33b7d40b793
Anja Kammer <post@anjakammer.de>
2017-11-23 00:04:24 +0100

    test validation of finished sprints #TDB-44

--- spec/models/story_spec.rb
+++ spec/models/story_spec.rb
@@ -5,4 +5,29 @@ RSpec.describe Story, type: :model do
   it { should have_many(:tasks).dependent(:destroy) }
   # Validation tests
   it { should validate_presence_of(:title) }
+
+  it "is not valid if new assigned Sprint is finished" do
+    subject = create(:story)
+    finished_sprint = create(
+        :sprint,
+        course_id: create(:course).id,
+        start_date: Date.new(2001,2,3),
+        end_date: Date.new(2001,9,3)
+    )
+    subject.sprint_id = finished_sprint.id
+    expect(subject).not_to be_valid
+  end
+
+  it "is valid if new assigned Sprint is not finished" do
+    subject = create(:story)
+    sprint = create(
+        :sprint,
+        course_id: create(:course).id,
+        start_date: Date.yesterday,
+        end_date: Date.tomorrow
+    )
+    subject.sprint_id = sprint.id
+    expect(subject).to be_valid
+  end
+
 end
