8740c8111b07965428fdf9b86e506f8c2699067f
Anja Kammer <post@anjakammer.de>
2017-11-22 23:29:28 +0100

    validate that story can be only edited for not-finished sprints #TDB-44

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,14 +1,17 @@
 class Story < ApplicationRecord
   has_many :tasks, dependent: :destroy
-  after_save :set_identifier
-  before_validation :set_status
-  acts_as_list
 
   def self.statuses
     {:open => 0, :progressing => 1, :closed => 2, :canceled => 3}
   end
 
+  acts_as_list
+
+  after_save :set_identifier
+  before_validation :set_status
+
   validates_presence_of :title, :status
+  validate :can_be_edited
 
   private
 
@@ -23,4 +26,24 @@ class Story < ApplicationRecord
       self.status = Story.statuses[:open]
     end
   end
+
+  def can_be_edited
+    if sprint_id_was.present?
+      old_sprint = Sprint.find_by!(id: sprint_id_was)
+      is_sprint_finished(old_sprint)
+    end
+
+    if sprint_id.present?
+      sprint = Sprint.find_by!(id: sprint_id)
+      is_sprint_finished(sprint)
+    end
+  end
+
+  def is_sprint_finished(sprint)
+    if sprint.end_date < Date.today
+      errors.add(:sprint_id, 'was finished and cannot be changed')
+      raise ActiveRecord::RecordInvalid.new(self)
+    end
+  end
+
 end
