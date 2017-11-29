fa84399b49752042ec3fba7e838afa3e89d8d8f4
Anja Kammer <post@anjakammer.de>
2017-11-29 15:28:04 +0100

    add sprint collection by date-range and sprint-duration #TDB-68

--- app/controllers/sprints_controller.rb
+++ app/controllers/sprints_controller.rb
@@ -1,11 +1,33 @@
 class SprintsController < ApplicationController
-  before_action :set_course, only: [:create]
+  before_action :set_course, only: [:create, :create_collection]
   before_action :set_sprint, only: [:update, :destroy]
 
   # POST /courses/course_id/sprints
   def create
     @sprint = @course.sprints.create!(sprint_params)
     json_response(@sprint, :created)
+  end
+
+  # POST /courses/course_id/sprint-collection
+  def create_collection
+    sprints = []
+    duration = params[:duration].to_i
+    start_date = params[:start_date].to_date
+    end_date = params[:end_date].to_date
+    days = (start_date...end_date).count
+    number_of_sprints = days/duration
+    number_of_sprints.times do |i|
+      end_date = start_date + duration.days
+      sprint = @course.sprints.create!(
+          { :name => "#{i+1}. Sprint",
+            :start_date => start_date,
+            :end_date => end_date
+          }
+      )
+      sprints.append(sprint)
+      start_date = end_date
+    end
+    json_response(sprints)
   end
 
   # PUT /sprints/:id
@@ -25,7 +47,7 @@ class SprintsController < ApplicationController
 
   def sprint_params
     # whitelist params
-    params.permit(:name, :end_date, :start_date)
+    params.permit(:name, :end_date, :start_date, :duration)
   end
 
   def set_course
