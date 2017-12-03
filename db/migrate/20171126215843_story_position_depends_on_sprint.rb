002fc0a6e1f8ece748accf9efbdf2643f8c7d2b9
Anja Kammer <post@anjakammer.de>
2017-12-03 21:05:01 +0100

    Fixed broken migration script #TDB-45

--- db/migrate/20171126215843_story_position_depends_on_sprint.rb
+++ db/migrate/20171126215843_story_position_depends_on_sprint.rb
@@ -1,9 +1,9 @@
 class StoryPositionDependsOnSprint < ActiveRecord::Migration[5.1]
   def change
-    Story.each do |story|
+    Story.all.each do |story|
       story.position = 0
     end
-    Story.order(:created_at).reverse.each do |story|
+    Story.all.order(:created_at).reverse.each do |story|
       story.move_to_bottom
     end
   end
