9835c040a81d955effcbdf2e412bda82d2e32161
Laura Seidler <laura.seidler@gmx.de>
2017-12-13 15:37:38 +0100

    Remove docker-compose override #TDB-57

--- .travis/docker-deploy.sh
+++ .travis/docker-deploy.sh
@@ -18,11 +18,8 @@ exit_on_error () {
 }
 
 echo "copying docker-compose files to $DEPLOYMENT_HOST"
-ssh -i id_rsa_${DEPLOYMENT_ENVIRONMENT} ${DEPLOYMENT_USER}@${DEPLOYMENT_HOST} 'mkdir -p ~/.docker'
-scp -i id_rsa_${DEPLOYMENT_ENVIRONMENT} .docker/variables.env ${DEPLOYMENT_USER}@${DEPLOYMENT_HOST}:~/.docker/variables.env
-scp -i id_rsa_${DEPLOYMENT_ENVIRONMENT} .docker/variables.production.env ${DEPLOYMENT_USER}@${DEPLOYMENT_HOST}:~/.docker/variables.production.env
-scp -i id_rsa_${DEPLOYMENT_ENVIRONMENT} .docker/docker-compose.production.yml ${DEPLOYMENT_USER}@${DEPLOYMENT_HOST}:~/.docker/docker-compose.production.yml
-scp -i id_rsa_${DEPLOYMENT_ENVIRONMENT} docker-compose.yml ${DEPLOYMENT_USER}@${DEPLOYMENT_HOST}:~
+scp -i id_rsa_${DEPLOYMENT_ENVIRONMENT} .docker/variables.production.env ${DEPLOYMENT_USER}@${DEPLOYMENT_HOST}:~
+scp -i id_rsa_${DEPLOYMENT_ENVIRONMENT} .docker/docker-compose.production.yml ${DEPLOYMENT_USER}@${DEPLOYMENT_HOST}:~
 exit_on_error $?
 
 echo "copying 04-deploy-steps-on-host.sh to server"
