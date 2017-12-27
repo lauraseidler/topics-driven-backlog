c61aabdd5842a24e4e57b842467e3d5535043ff2
Laura Seidler <laura.seidler@gmx.de>
2017-12-27 11:43:19 +0100

    Make FE tests run on Travis #TDB-59

--- .travis/01-test.sh
+++ .travis/01-test.sh
@@ -12,15 +12,16 @@ if [ $? != 0 ]; then
     exit 1
 else
     echo "--------- docker exec -ti postgresql-dev netstat -nlp | grep 5432"
-    docker exec -ti postgresql-dev netstat -nlp | grep 5432
+    docker-compose exec postgresql netstat -nlp | grep 5432
 
     echo "done building and starting image"
     docker ps
 
     echo "starting tests in docker image"
-    docker exec -ti tdb-dev .docker/wait-for-db.sh
-    docker exec -e RAILS_ENV=test -ti tdb-dev rake db:create RAILS_ENV=test
-    docker exec -e RAILS_ENV=test -ti tdb-dev rake db:migrate RAILS_ENV=test
-    docker exec -e RAILS_ENV=test -ti tdb-dev rake db:migrate:status RAILS_ENV=test
-    docker exec -e RAILS_ENV=test -ti tdb-dev rspec spec
+    docker-compose exec app .docker/wait-for-db.sh
+    docker-compose exec app rake db:create RAILS_ENV=test
+    docker-compose exec app rake db:migrate RAILS_ENV=test
+    docker-compose exec app rake db:migrate:status RAILS_ENV=test
+    docker-compose exec app rspec spec
+    docker-compose exec app ./node_modules/.bin/jest
 fi
