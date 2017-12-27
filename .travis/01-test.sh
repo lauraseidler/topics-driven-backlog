ded2edd4af6db7b5bedcbf6185392fbf14abb90e
Laura Seidler <laura.seidler@gmx.de>
2017-12-27 14:04:05 +0100

    Fix docker commands in test script #TDB-59

--- .travis/01-test.sh
+++ .travis/01-test.sh
@@ -15,22 +15,41 @@ else
     docker-compose exec postgresql netstat -nlp | grep 5432
 
     echo "done building and starting image"
+
+
+    echo "waiting for db"
     docker ps
     docker-compose config --services
-
-    echo "starting tests in docker image"
     docker-compose exec app .docker/wait-for-db.sh
 
+    echo "creating db"
     docker ps
     docker-compose config --services
-
     docker-compose exec app rake db:create RAILS_ENV=test
+
+    echo "migrating db"
+    docker ps
+    docker-compose config --services
     docker-compose exec app rake db:migrate RAILS_ENV=test
+
+    echo "db migration status"
+    docker ps
+    docker-compose config --services
     docker-compose exec app rake db:migrate:status RAILS_ENV=test
+
+    echo "running rspec tests"
+    docker ps
+    docker-compose config --services
     docker-compose exec app rspec spec
+
+    echo "running jest tests"
+    docker ps
+    docker-compose config --services
     docker-compose exec app ./node_modules/.bin/jest
 
     echo "uploading coverage reports"
+    docker ps
+    docker-compose config --services
     bash <(curl -s https://codecov.io/bash) -f coverage/.resultset.json -cF rails
     bash <(curl -s https://codecov.io/bash) -f test/__coverage__/coverage-final.json -cF javascript
 fi
