d213eb8de9b05709196252789649a226298fe042
Laura Seidler <laura.seidler@gmx.de>
2017-12-27 12:59:28 +0100

    Fix docker commands in test script #TDB-59

--- .travis/01-test.sh
+++ .travis/01-test.sh
@@ -2,25 +2,28 @@
 
 echo "$0: starting build"
 
+echo "--------- sudo netstat -nlp | grep 5432"		
+sudo netstat -nlp | grep 5432
+
 docker-compose up -d
 
 if [ $? != 0 ]; then
     echo "ERROR: docker-compose up -d FAILED"
     exit 1
 else
-    echo "--------- docker-compose exec postgresql netstat -nlp | grep 5432"
-    docker-compose exec postgresql netstat -nlp | grep 5432
+    echo "--------- docker exec -ti tdb-dev-postgres netstat -nlp | grep 5432"
+    docker exec -ti tdb-dev-postgres netstat -nlp | grep 5432
 
     echo "done building and starting image"
     docker ps
 
     echo "starting tests in docker image"
-    docker exec tdb-dev-app .docker/wait-for-db.sh
-    docker exec tdb-dev-app rake db:create RAILS_ENV=test
-    docker exec tdb-dev-app rake db:migrate RAILS_ENV=test
-    docker exec tdb-dev-app rake db:migrate:status RAILS_ENV=test
-    docker exec tdb-dev-app rspec spec
-    docker exec tdb-dev-app ./node_modules/.bin/jest
+    docker exec -ti tdb-dev-app .docker/wait-for-db.sh
+    docker exec -ti tdb-dev-app rake db:create RAILS_ENV=test
+    docker exec -ti tdb-dev-app rake db:migrate RAILS_ENV=test
+    docker exec -ti tdb-dev-app rake db:migrate:status RAILS_ENV=test
+    docker exec -ti tdb-dev-app rspec spec
+    docker exec -ti tdb-dev-app ./node_modules/.bin/jest
 
     echo "uploading coverage reports"
     bash <(curl -s https://codecov.io/bash) -f coverage/.resultset.json -cF rails
