e56fc840b6d0edfff19b4eabb62d500eafcd80a3
Laura Seidler <laura.seidler@gmx.de>
2017-12-27 14:37:46 +0100

    Move yarn install to pass integrity checks #TDB-59

--- .travis/01-test.sh
+++ .travis/01-test.sh
@@ -18,39 +18,26 @@ else
 
 
     echo "waiting for db"
-    docker ps
-    docker-compose config --services
     docker-compose exec app .docker/wait-for-db.sh
 
     echo "creating db"
-    docker ps
-    docker-compose config --services
     docker-compose exec app rake db:create RAILS_ENV=test
 
-    docker-compose logs
     echo "migrating db"
-    docker ps
-    docker-compose config --services
     docker-compose exec app rake db:migrate RAILS_ENV=test
 
     echo "db migration status"
-    docker ps
-    docker-compose config --services
     docker-compose exec app rake db:migrate:status RAILS_ENV=test
 
     echo "running rspec tests"
-    docker ps
-    docker-compose config --services
     docker-compose exec app rspec spec
 
     echo "running jest tests"
-    docker ps
-    docker-compose config --services
     docker-compose exec app ./node_modules/.bin/jest
 
     echo "uploading coverage reports"
-    docker ps
-    docker-compose config --services
     bash <(curl -s https://codecov.io/bash) -f coverage/.resultset.json -cF rails
     bash <(curl -s https://codecov.io/bash) -f test/__coverage__/coverage-final.json -cF javascript
+
+    docker-compose logs
 fi
--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-backend: rm -f tmp/pids/server.pid && rails s --port 80 --binding 0.0.0.0
-frontend: yarn && webpack-dev-server --hot
+backend: yarn install && rm -f tmp/pids/server.pid && rails s --port 80 --binding 0.0.0.0
+frontend: webpack-dev-server --hot
