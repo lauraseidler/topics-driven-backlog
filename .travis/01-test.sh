3b5a38821222565376515be8cdd917340551d10c
Laura Seidler <laura.seidler@gmx.de>
2017-12-27 12:01:50 +0100

    Upload coverage to codecov #TDB-59

--- .travis/01-test.sh
+++ .travis/01-test.sh
@@ -2,16 +2,13 @@
 
 echo "$0: starting build"
 
-echo "--------- sudo netstat -nlp | grep 5432"
-sudo netstat -nlp | grep 5432
-
 docker-compose up -d
 
 if [ $? != 0 ]; then
     echo "ERROR: docker-compose up -d FAILED"
     exit 1
 else
-    echo "--------- docker exec -ti postgresql-dev netstat -nlp | grep 5432"
+    echo "--------- docker-compose exec postgresql netstat -nlp | grep 5432"
     docker-compose exec postgresql netstat -nlp | grep 5432
 
     echo "done building and starting image"
@@ -24,4 +21,7 @@ else
     docker-compose exec app rake db:migrate:status RAILS_ENV=test
     docker-compose exec app rspec spec
     docker-compose exec app ./node_modules/.bin/jest
+
+    echo "uploading coverage reports"
+    bash <(curl -s https://codecov.io/bash) -f test/__coverage__/coverage-final.json -cF javascript
 fi
