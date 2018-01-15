16567a4310eaf82c22a990f55b44e5e174978a43
Anja Kammer <post@anjakammer.de>
2018-01-15 02:21:31 +0100

    added users model+test #TDB-104

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180113141102) do
+ActiveRecord::Schema.define(version: 20180115005556) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,6 +92,12 @@ ActiveRecord::Schema.define(version: 20180113141102) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["course_id"], name: "index_topics_on_course_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
   end
 
   add_foreign_key "project_positions", "projects", on_delete: :cascade
