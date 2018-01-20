c6ae20c7f0b38751b26578cad21068008cc5f487
Anja Kammer <post@anjakammer.de>
2018-01-20 22:36:50 +0100

    removed auth for frontend route test #TDB-131

--- spec/controllers/frontend_controller_spec.rb
+++ spec/controllers/frontend_controller_spec.rb
@@ -2,12 +2,6 @@ require 'rails_helper'
 
 RSpec.describe FrontendController, type: :controller do
 
-  let(:user) { create(:user) }
-  before(:each) do
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-    allow_any_instance_of(ApplicationController).to receive(:authorize_request).and_return(user)
-  end
-
   describe "GET #index" do
     before { get :index }
 
