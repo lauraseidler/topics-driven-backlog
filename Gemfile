2c4406128f4783d7ccbdde76b06cf2fe17e86798
Anja Kammer <post@anjakammer.de>
2018-01-20 01:25:17 +0100

    JWT en and decoding and exception handling #TDB-131

--- Gemfile
+++ Gemfile
@@ -19,6 +19,8 @@ gem 'puma', '~> 3.7'
 # gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.5'
+# JSON Web Token
+gem 'jwt'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -2,6 +2,10 @@ module ExceptionHandler
   # provides the more graceful `included` method
   extend ActiveSupport::Concern
 
+  class AuthenticationError < StandardError; end
+  class MissingToken < StandardError; end
+  class InvalidToken < StandardError; end
+
   def raise_exception_on_validation_error(errors)
     errors = errors - ['', nil]
 
@@ -11,17 +15,35 @@ module ExceptionHandler
   end
 
   included do
-    rescue_from ActiveRecord::RecordNotFound do |e|
-      json_response({ message: e.message }, :not_found)
-    end
-
-    rescue_from ActiveRecord::RecordInvalid do |e|
-      json_response({ message: e.message }, :unprocessable_entity)
-    end
-
-    rescue_from ActionController::BadRequest do |e|
-      json_response({ message: e.message }, :bad_request)
-    end
-
+    rescue_from ActionController::BadRequest, with: :bad_request
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+    rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
+    rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
+    rescue_from ExceptionHandler::AuthenticationError, with: :unauthorized_request
+    rescue_from ExceptionHandler::MissingToken, with: :record_invalid
+    rescue_from ExceptionHandler::InvalidToken, with: :record_invalid
   end
+
+  private
+
+  # JSON response with message; Status code 400 - bad request
+  def bad_request(e)
+    json_response({ message: e.message }, :bad_request)
+  end
+
+  # JSON response with message; Status code 401 - Unauthorized
+  def unauthorized_request(e)
+    json_response({ message: e.message }, :unauthorized)
+  end
+
+  # JSON response with message; Status code 404 - record not found
+  def record_not_found(e)
+    json_response({ message: e.message }, :not_found)
+  end
+
+  # JSON response with message; Status code 422 - unprocessable entity
+  def record_invalid(e)
+    json_response({ message: e.message }, :unprocessable_entity)
+  end
+
 end
