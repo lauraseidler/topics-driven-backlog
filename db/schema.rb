32946ca179d5925e45313c2accb7a0e0f3a10b59
Anja Kammer <post@anjakammer.de>
2018-01-22 00:38:28 +0100

    Added role column, and default value #TDB-168

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180120182857) do
+ActiveRecord::Schema.define(version: 20180121232727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -112,6 +112,7 @@ ActiveRecord::Schema.define(version: 20180120182857) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
+    t.integer "role", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
