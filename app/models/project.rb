3a1d2d1659c2f40dd5aaeaea95bb693faded3334
Anja Kammer <post@anjakammer.de>
2018-01-23 01:33:29 +0100

    Changes memberships model to join-table #TDB-143

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,8 +1,7 @@
 class Project < ApplicationRecord
   belongs_to :course
   has_many :stories, dependent: :destroy
-  has_many :memberships
-  has_many :users, through: :memberships
+  has_and_belongs_to_many :users
 
   validates_presence_of :title, :course_id
   validates :title, uniqueness: { scope: :course_id }
