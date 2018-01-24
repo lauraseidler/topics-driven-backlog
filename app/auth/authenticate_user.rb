64350333b7cc4188f65be515e4d2b78812a6657e
Anja Kammer <post@anjakammer.de>
2018-01-25 00:03:01 +0100

    refactores get_user_group #TDB-132

--- app/auth/authenticate_user.rb
+++ app/auth/authenticate_user.rb
@@ -36,12 +36,13 @@ class AuthenticateUser
   # verify user credentials
   def get_user(email, password)
     if email.present? && password.present?
-      user_role = get_user_role_via_ldap(username(email), password)
-      user = User.find_by(email: email, role: user_role)
+      ldap_user_role = get_user_role_via_ldap(username(email), password)
+      user = User.find_by(email: email)
 
       if user.nil?
-        # TODO set role if user_role changed on ldap
-        user = User.create!(email: email, role: user_role)
+        user = User.create!(email: email, role: ldap_user_role)
+      elsif user.role != ldap_user_role
+        user.update_attribute(role: ldap_user_role)
       end
 
       return user if user
@@ -51,8 +52,9 @@ class AuthenticateUser
   end
 
   def get_user_role_via_ldap(username, password)
+    query_result = []
     begin
-      ldap = Net::LDAP.new(
+      Net::LDAP.open(
           host: LDAP_HOST,
           port: LDAP_PORT,
           encryption:
@@ -60,43 +62,34 @@ class AuthenticateUser
                   method: :simple_tls,
                   verify_mode: OpenSSL::SSL::VERIFY_NONE
               },
+          base: LDAP_CONNECTSTRING,
           auth: {
               method: :simple,
-              username: "CN=#{username},#{LDAP_CONNECTSTRING}",
+              username: username,
               password: password
           }
-      )
+      ) do |ldap|
+        query_result = ldap.search(
+            :base => LDAP_CONNECTSTRING,
+            :filter => Net::LDAP::Filter.eq( 'CN', username )
+        )
+      end
+
+      get_user_role(query_result)
     rescue
       raise(ExceptionHandler::AuthenticationServerIsDown, Message.contact_the_admin)
     end
-
-    authenticate_user_via_ldap(ldap)
-    get_user_role(ldap, username)
   end
 
-  def authenticate_user_via_ldap(ldap)
-    authenticated = ldap.bind
+  def get_user_role(query_result)
+    if query_result.size === 1
+      user_groups = query_result[0]['memberof'].split(',')
 
-    return authenticated if authenticated
-    raise(ExceptionHandler::AuthenticationError, Message.invalid_credentials)
-  end
-
-  def get_user_role(ldap, username)
-    filter = Net::LDAP::Filter.eq( "CN", "#{username}" )
-    query_result = ldap.search( :base => LDAP_CONNECTSTRING, :filter => filter )
-
-    if query_result.size != 1
-      raise(ExceptionHandler::AuthenticationError, Message.invalid_credentials)
-    end
-
-    user_groups = query_result[0]['memberof'].split(',')
-
-    user_groups.split('=').each do |group, value|
-      if group == 'DN' && value == HTW_USER_TYPE_INSTRUCTOR
+      if user_groups.include?('DN=' . HTW_USER_TYPE_INSTRUCTOR)
         return User.roles[:instructor]
       end
 
-      if group == 'DN' && value == HTW_USER_TYPE_STUDENT
+      if user_groups.include?('DN=' . HTW_USER_TYPE_STUDENT)
         return User.roles[:student]
       end
     end
