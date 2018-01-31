f6f4d5afe08bc8eb2801b3648e9c92f8adb45987
Anja Kammer <post@anjakammer.de>
2018-02-01 00:58:34 +0100

    cleanup from base #TDB-165

--- spec/requests/projects_spec.rb
+++ spec/requests/projects_spec.rb
@@ -33,7 +33,6 @@ RSpec.describe 'Projects API' do
 
       it 'returns all course projects' do
         expect(json['projects'].size).to eq(20)
-        print(json['projects'][0])
         expect(json['projects'][0]['permissions']).to eq(expected_permissions)
       end
     end
