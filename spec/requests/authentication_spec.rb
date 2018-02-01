f996b7e7cb032c01c2c8f07417daa64308467954
Anja Kammer <post@anjakammer.de>
2018-02-01 22:12:33 +0100

    added /become-student route, returns user #TDB-180

--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -12,10 +12,10 @@ RSpec.describe 'Authentication', type: :request do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
     allow_any_instance_of(LdapAuthenticator).to receive(:call).and_return(User.roles[:instructor])
   end
+  let(:headers) { valid_headers.except('Authorization') }
 
   # Test suite for POST /get-token
   describe 'POST /get-token' do
-    let(:headers) { valid_headers.except('Authorization') }
     let(:valid_credentials) {
       {
           email: user.email.to_s,
@@ -58,5 +58,33 @@ RSpec.describe 'Authentication', type: :request do
         expect(response.body).to match(/#{Message::invalid_credentials}/)
       end
     end
+  end
+
+  # Test suite for POST /become-student
+  describe 'POST /become-student' do
+    before {
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      allow_any_instance_of(ApplicationController).to receive(:authorize_request).and_return(user)
+    }
+    context 'When request is valid' do
+      before {post '/become-student'}
+      let(:expected_permissions) {
+        {
+            'courses' => {'read' => true, 'create' => false}
+        }
+      }
+      it 'returns the user object' do
+        expect(json['user']).not_to be_nil
+        expect(json['user']['email']).to eq(user.email)
+        expect(json['user']['id']).to eq(user.id)
+        expect(json['user']['role']).to eq(User.roles[:student])
+        expect(json['user']['permissions']).to eq(expected_permissions)
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
   end
 end
